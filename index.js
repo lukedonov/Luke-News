52a9debf8f5a7fef3165b56a92e4aaab2ee35447
Luke Donovan <lukelamnisos@icloud.com>
2020-05-14 12:42:34 +0100

    added express server

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const express = require("express");
-
+const express = require('express');
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is listening on port: 3000");
+const http = require('http');
+const server = http.createServer();
+server.on('request', (app)); 
+//const server = require('http').createServer(app);
+
+server.listen(8080, 'localhost', function () {
+  console.log('Server listening on port 8080')
+})
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
+

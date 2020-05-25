530115202a0065c62ac4e778dad9740eb675976c
Luke Donovan <lukelamnisos@icloud.com>
2020-05-25 15:20:10 +0100

    added

--- src/app/ArticleSummary.js
+++ src/app/ArticleSummary.js
@@ -7,7 +7,7 @@
     const apiUrl = `/api/summary/${encodeURIComponent(url)}`
     const response = await fetch(apiUrl)
     const json = await response.json()
-    document.getElementById("stories").innerHTML = `<div><h1>${headline}</h1><img src='${img}'><div class='summarybody'>${json.text}</div><a href='${url}'class='articleUrl'>Link to full article</a><a href='/'class='link'>Back to stories</a></div>`
+    document.getElementById("stories").innerHTML = `<div><h1>${headline}</h1><img src='${img}'><div class='summarybody'>${json.text.replace(/Dominic/gi, "Carlo").replace(/Cummings/gi, "Kureishi").replace(/Boris/gi,"Eddy").replace(/Johnson/gi, "Gray")}</div><a href='${url}'class='articleUrl'>Link to full article</a><a href='/'class='link'>Back to stories</a></div>`
   }
 
   exports.ArticleSummary = ArticleSummary
